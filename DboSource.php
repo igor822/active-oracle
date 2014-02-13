e687b607956836db59ce734c078a2fbb463bb627
Igor Carvalho <igor.carvalho@work.srv.br>
2014-02-13 15:25:09 -0200

    Added comments to class for docs and a new condition to verify if connector's name is passed

--- DboSource.php
+++ DboSource.php
@@ -11,12 +11,24 @@ class DboSource implements ConnectorInterface {
 
 	private static $_instance = null;
 
+	/**
+	 * Constructor of object
+	 *
+	 * @param {array} $dataSource options of connection, is optional
+	 * @return {object} instance of object
+	 */
 	public function __construct($dataSource = null) {
 		if (!empty($dataSource['connector']) && $dataSource['connector'] != '') {
 			$this->setConnector($dataSource['connector']);
 		} 
 	}
 
+	/**
+	 * Singleton to get an instance of object
+	 * 
+	 * @param {array} $dataSource options of connection, is optional
+	 * @return {object} $instance
+	 */
 	public static function connect($dataSource = null) {
 		if (empty(self::$_instance)) {
 			$obj = __CLASS__;
@@ -26,15 +38,31 @@ class DboSource implements ConnectorInterface {
 		return self::$_instance;
 	}
 
+	/**
+	 * Method to set a new connector data and get a new instance of object
+	 *
+	 * @param {string} Connector name by name of database, like, Oracle, MySql
+	 * @access public
+	 * @throws ConnectorException
+	 * @return {object} $this
+	 */
 	public function setConnector($connector) {
 		try {
-			$ns_class_name = __NAMESPACE__.'\\'.'Connector'.'\\'.(ucfirst($connector).'Connector');
-			$this->_connector = new $ns_class_name();
+			if (is_array($connector) && count($connector) > 0) {
+				$ns_class_name = __NAMESPACE__.'\\'.'Connector'.'\\'.(ucfirst($connector).'Connector');
+				$this->_connector = new $ns_class_name();
+			} else throw new ConnectorException('Connector not found', 1001);
 		} catch(Exception $e) {
 			var_dump($e->getMessage());
 		}
+		return $this;
 	}
 
+	/**
+	 * Retrieve the instance of connector
+	 * 
+	 * @return {object} $connector instance of connector
+	 */
 	public function getConnector() {
 		if (!empty($this->_connector)) return $this->_connector;
 	}
