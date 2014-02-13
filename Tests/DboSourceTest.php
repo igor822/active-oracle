8ef0eaa8b58c36b98d23dd13762b2d50a73f265e
Igor Carvalho <igor.carvalho@work.srv.br>
2014-02-13 15:00:55 -0200

    Added annotation of runInSeparateProcess

--- Tests/DboSourceTest.php
+++ Tests/DboSourceTest.php
@@ -7,9 +7,26 @@ require_once '../../autoload.php';
 
 class DboSourceTest extends \PHPUnit_Framework_TestCase {
 
-	public function testStaticInstanceOf() {
-		$dbo = DboSource::connect(array());
+	/**
+	 * @before
+	 * @runInSeparateProcess
+	 */
+	public function testInstanceOf() {
+		$dbo = DboSource::connect();
 		$this->assertInstanceOf('DataSource\DboSource', $dbo);
+
+		$dbo = new DboSource();
+		$this->assertInstanceOf('DataSource\DboSource', $dbo);
+	}
+
+	/**
+	 * 
+	 */
+	public function testInstanceOfConnector() {
+		$dbo_conn = DboSource::connect(array('connector' => 'oracle'));
+		$connector = $dbo_conn->getConnector();
+		
+		$this->assertInstanceOf('DataSource\Connector\OracleConnector', $connector);
 	}
 
 }
