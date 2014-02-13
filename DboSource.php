ab4b2207972d4589a55440405c662530840e4192
Igor Carvalho <igor.carvalho@work.srv.br>
2014-02-13 11:47:28 -0200

    Criada classe para auxiliar na chamada de conectores e criação de alguns métodos como callbacks

---
+++ DboSource.php
@@ -0,0 +1,18 @@
+<?php
+namespace DataSource\DboSource;
+
+use DataSource\ConnectorInterface;
+
+class DboSource implements ConnectorInterface {
+
+	private $_dataSource = array();
+
+	public static function connect($DataSource) {
+
+	}
+
+	public function getConnector() {
+		
+	}
+
+}
