08629a230897188d7107dd5f917964b107516b20
Igor Carvalho <igor.carvalho@work.srv.br>
2014-02-13 13:14:38 -0200

    Correct comparison of instance

--- DboSource.php
+++ DboSource.php
@@ -1,5 +1,5 @@
 <?php
-namespace DataSource\DboSource;
+namespace DataSource;
 
 use DataSource\ConnectorInterface;
 
@@ -7,12 +7,32 @@ class DboSource implements ConnectorInterface {
 
 	private $_dataSource = array();
 
-	public static function connect($DataSource) {
+	private $_connector = null;
+
+	private static $_instance = null;
+
+	public function __construct($dataSource = null) {
+		if (!empty($dataSource['connector']) && $dataSource['connector'] != '') $this->setConnector($connector);
+	}
+
+	public static function connect($dataSource = null) {
+		if (empty(self::$_instance)) {
+			$obj = __CLASS__;
+			self::$_instance = new $obj($dataSource);
+		}
+
+		return self::$_instance;
+	}
+
+	public function setConnector($connector) {
+		$ns = __NAMESPACE__;
+		$cn = ucfirst($connector).'Connector';
+		$this->_connector = new $ns.'\\'.'Connector'.'\\'.$cn();
 
 	}
 
 	public function getConnector() {
-		
+		if (!empty($this->_connector)) return $this->_connector;
 	}
 
 }
