5baab2530c51796d1c47d62339e92a8d9cb7e88c
Igor Carvalho <igor.carvalho@work.srv.br>
2014-02-13 11:52:07 -0200

    Renomeado classe de teste e atualizado namespace para chamada da classe OracleConnector

--- Tests/OracleConnectorTest.php
+++ Tests/OracleConnectorTest.php
@@ -1,17 +1,17 @@
 <?php
-namespace DataSource\OraConnection\Tests;
+namespace DataSource\Tests;
 
-use DataSource\OraConnection;
+use DataSource\Connector\OracleConnector;
 
 require_once '../../autoload.php';
 
-//require_once '../OraConnection.php';
+//require_once '../OracleConnector.php';
 
-class OraConnectionTest extends \PHPUnit_Framework_TestCase {
+class OracleConnectorTest extends \PHPUnit_Framework_TestCase {
 
 	public function testIfInstanceOf() {
-		$ora_conn = OraConnection::connect();
-		$this->assertInstanceOf('DataSource\OraConnection', $ora_conn);
+		$ora_conn = OracleConnector::connect();
+		$this->assertInstanceOf('DataSource\Connector\OracleConnector', $ora_conn);
 
 		return $ora_conn;
 	}
@@ -19,7 +19,7 @@ class OraConnectionTest extends \PHPUnit_Framework_TestCase {
 	/**
 	 * @depends testIfInstanceOf
 	 */
-	public function testCheckIfGetInstanceIsTheSame(OraConnection $ora_conn = null) {
+	public function testCheckIfGetInstanceIsTheSame(OracleConnector $ora_conn = null) {
 		$this->assertNotEmpty($ora_conn);
 
 		$id = spl_object_hash($ora_conn);
@@ -31,7 +31,7 @@ class OraConnectionTest extends \PHPUnit_Framework_TestCase {
 	/**
 	 * @depends testCheckIfGetInstanceIsTheSame
 	 */
-	public function testOpenCloseConnection(OraConnection $ora_conn = null) {
+	public function testOpenCloseConnection(OracleConnector $ora_conn = null) {
 		$dataSource = array(
 			'username' => 'hr',
 			'password' => 'root',
@@ -51,7 +51,7 @@ class OraConnectionTest extends \PHPUnit_Framework_TestCase {
 	/**
 	 * @depends testCheckIfGetInstanceIsTheSame
 	 */
-	public function testSomeQuery(OraConnection $ora_conn = null) {
+	public function testSomeQuery(OracleConnector $ora_conn = null) {
 		$query = 'select * from hr.regions where rownum < 10';
 
 		$dataSource = array(
@@ -70,7 +70,7 @@ class OraConnectionTest extends \PHPUnit_Framework_TestCase {
 
 		$this->assertNotEmpty($result);
 		$this->assertInternalType('array', $result);
-		
+
 	}
 
 }
