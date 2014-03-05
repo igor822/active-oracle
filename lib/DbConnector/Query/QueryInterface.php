8d03b7dfda03b2192cce80998269060c50382985
Igor Carvalho <igor.carvalho@work.srv.br>
2014-03-05 13:24:34 -0300

    Added default signatures of methods

--- lib/DbConnector/Query/QueryInterface.php
+++ lib/DbConnector/Query/QueryInterface.php
@@ -5,4 +5,18 @@ interface QueryInterface {
 
 	public function select($fields = '*');
 
+	public function from($table = null);
+
+	public function where($condition, $param);
+
+	public function join($table, $on, $type = 'inner');
+
+	public function order($order);
+
+	public function group($group);
+
+	public function joinAll();
+
+	public function getSql();
+
 }
