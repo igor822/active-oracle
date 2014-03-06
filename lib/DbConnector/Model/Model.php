bc22fbd57056b7230196a8db88f7ddf9033a6791
Igor Carvalho <igor.carvalho@work.srv.br>
2014-03-06 16:35:40 -0300

    Implemented method to insert and update from model

--- lib/DbConnector/Model/Model.php
+++ lib/DbConnector/Model/Model.php
@@ -3,6 +3,8 @@ namespace DbConnector\Model;
 
 use DbConnector\DboSource;
 use DbConnector\Query\Query;
+use DbConnector\Query\Insert;
+use DbConnector\Query\Update;
 
 class Model extends DboSource {
 
@@ -12,6 +14,12 @@ class Model extends DboSource {
 
 	protected $connector = null;
 
+	protected $insertAdp = null;
+
+	protected $updateAdp = null;
+
+	protected $alias = null;
+
 	public function __construct($dataSource) {
 		parent::__construct($dataSource);
 		$this->connector = $this->getConnector()->openConnection();
@@ -19,7 +27,7 @@ class Model extends DboSource {
 	}
 
 	public function getQueryAdp() {
-		if ($this->queryAdp === null) $this->queryAdp = new Query(array('table' => $this->table));
+		if ($this->queryAdp === null) $this->queryAdp = new Query(array('table' => $this->table, 'alias' => $this->alias));
 		return $this->queryAdp;
 	}
 
@@ -33,13 +41,14 @@ class Model extends DboSource {
 		return parent::connect($dataSource);
 	}
 
-	private function buildQuery($options) {
-		if (!empty($options['fields'])) $this->queryAdp->select($options['fields']);
-		else $this->queryAdp->select();
+	private function buildQuery($options, &$adapter = null) {
+		if (null === $adapter) $adapter = &$this->queryAdp;
+		if (!empty($options['fields'])) $adapter->select($options['fields']);
+		else $adapter->select();
 
-		if (!empty($options['conditions'])) $this->buildConditions($options['conditions']);
-		if (!empty($options['order'])) $this->queryAdp->order($options['order']);
-		if (!empty($options['group'])) $this->queryAdp->group($options['group']);
+		if (!empty($options['conditions'])) $this->buildConditions($options['conditions'], $adapter);
+		if (!empty($options['order'])) $adapter->order($options['order']);
+		if (!empty($options['group'])) $adapter->group($options['group']);
 	}
 
 	/**
@@ -49,21 +58,57 @@ class Model extends DboSource {
 	*))
 	*/
 
-	private function buildConditions($conditions) {
+	private function buildConditions($conditions, &$adapter = null) {
+		if (null === $adapter) $adapter = &$this->queryAdp;
+
 		if (is_array($conditions) && count($conditions) > 0) {
 			foreach ($conditions as $key => $cond) {
 				if ('or' == strtolower($key)) {
 					if (is_array($cond)) {
-						foreach ($cond as $or) $this->queryAdp->orWhere($or);
+						foreach ($cond as $or) $adapter->orWhere($or);
 					}
 				} else {
 					if (is_array($cond)) {
-						foreach ($cond as $cd) $this->queryAdp->where($cd);
+						foreach ($cond as $cd) $adapter->where($cd);
 					} else {
-						$this->queryAdp->where($cond);
+						$adapter->where($cond);
 					}
 				}
 			}
 		}
+	}
+
+	/**
+	 * Method to insert data to table
+	 *
+	 * @access public
+	 * @param {array} $values Values to be added to table
+	 * @return boolean
+	 */ 
+	public function insert($values) {
+		if (null === $this->insertAdp) $this->insertAdp = new Insert(array('table' => $this->table, 'alias' => $this->alias));
+		$this->insertAdp->createQuery($values);
+		$sql = $this->insertAdp->getSql();
+		return $this->execute($sql);
+	}
+
+	/**
+	 * Method to update data
+	 *
+	 * @param {array} $values Values to be updated 
+	 * @param {array} $options Conditions of query to update
+	 * @return boolean
+	 */
+	public function update($values, $options = array()) {
+		if (null === $this->updateAdp) $this->updateAdp = new Update(array('table' => $this->table, 'alias' => $this->alias));
+
+		$this->updateAdp->setValues($values);
+
+		if (count($options) > 0) {
+			$this->buildQuery($options, $this->updateAdp);
+		}
+		$sql = $this->updateAdp->createQuery()->getSql();
+		//echo $sql; exit;
+		return $this->execute($sql);
 	}
 }
