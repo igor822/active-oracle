f0567e512fb93b00a12ef38c0a61fe280449df1b
Igor Carvalho <igor.carvalho@work.srv.br>
2014-03-26 15:59:44 -0300

    Adicionado variáveis de sistema

--- lib/autoload.php
+++ lib/autoload.php
@@ -1,4 +1,8 @@
 <?php
+putenv('ORACLE_HOME=/usr/lib/oracle/11.2/client64');
+putenv('LD_LIBRARY_PATH=/usr/lib/oracle/11.2/client64/lib');
+putenv('TNS_ADMIN=/usr/lib/oracle/11.2/client64/network/admin');
+
 if (!defined('BASE_PATH')) define('BASE_PATH', realpath(dirname(__FILE__)));
 
 function autoloader($class_name) {
